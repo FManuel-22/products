7bc7081fbd9bd3134066b5e4a2d10c820eb1053a
Olufemi Emmanuel <105301798+FManuel-22@users.noreply.github.com>
2023-01-12 19:59:49 +0100

    Add files via upload

---
+++ products/includes/class-products-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.femiproducts.com
+ * @since      1.0.0
+ *
+ * @package    Products
+ * @subpackage Products/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Products
+ * @subpackage Products/includes
+ * @author     Olufemi Emmanuel <uniqueinnovator924@gmail.com>
+ */
+class Products_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
